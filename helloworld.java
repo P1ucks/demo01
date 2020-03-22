a6933ce5086d1e765bd20b83927671dcf440b390
陈家祥 <p1ucks@chenjiaxiangdeMacBook-Pro.local>
2020-03-22 17:24:41 +0800

    helloworld

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld{
+	public static void main(String[] args){
+		System.out.print("Hello world");
+		System.out.print("1");
+		System.out.print("Hello world");
+		System.out.print("2");
+	}
+}
